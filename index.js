f771856c3148a13d89ffd2f87c157f5c92a1b781
Mwambui123 <maurinawambuia@gmail.com>
2023-10-04 21:36:21 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code in this file!
+// const currentUser = 'Grace Hopper';
+// const welcomeMessage = 'Welcome to Flatbook, currentUser';
+// typeof "const currentUser = 'Grace Hopper';";
+// //=> "string"
+
+// "const currentUser = 'Grace Hopper';".length;
+// //=> 35
+
+// currentUser;
+
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
